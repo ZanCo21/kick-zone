7f0985796f8e0fc97a5fbd12825be8e8a1fde48d
Bagas Saadih <saadihbagas@gmail.com>
2026-05-29 21:55:50 +0700

    " Menambahkan packages koneksi dan file koneksi.java"

---
+++ src/koneksi/koneksi.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package koneksi;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class koneksi {
+    public static Connection getConnection() {
+    try {
+        String url = "jdbc:mysql://localhost:3306/db_kick_zone";
+        String user = "root";
+        String pass = "";
+        
+        Connection conn = DriverManager.getConnection(url, user, pass);
+        return conn;
+    } catch (Exception e) {
+    System.out.println("Koneksi ke database gagal: " + e.getMessage());
+    return null;
+    }
+    }
+}
